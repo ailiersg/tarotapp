c360255674f0559935ae478c8fda066e4bba6fc9
feiye <feiye@feiyedeMacBook-Pro.local>
2019-10-26 11:18:55 +0800

    7

--- serve_test/Controllers/WeChatPayController.cs
+++ serve_test/Controllers/WeChatPayController.cs
@@ -11,41 +11,116 @@ namespace TodoApi.Controllers
 {
     public class weCharPayController : ControllerBase
     {
-        [HttpPost]
-        public string WxNotify()
+        ///<summary>
+        /// 微信支付回调地址
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("PayNotifyUrl")]
+        public ActionResult PayNotifyUrl()
         {
+            // ResponseResult result = new ResponseResult();
             ResponseHandler resHandler = new ResponseHandler(HttpContext);
-
-
             string return_code = resHandler.GetParameter("return_code");
             string return_msg = resHandler.GetParameter("return_msg");
-            string out_trade_no = resHandler.GetParameter("out_trade_no");//订单号
-            string total_fee = resHandler.GetParameter("total_fee");//订单金额，单位分
-            total_fee = (Convert.ToDecimal(total_fee) / 100).ToString("#0.00");//订单金额，单位元
+            Console.Write("return_msg=" + return_msg);
 
-            string res = null;
-
-
-            resHandler.SetKey("z76h28p279z76h28p279kidjs7dh82k1");
-            //验证请求是否从微信发过来（安全）
-            if (resHandler.IsTenpaySign())
+            try
             {
-                res = "success";
+
+                resHandler.SetKey("z76h28p279z76h28p279kidjs7dh82k1");
+
+                var notifyXml = resHandler.ParseXML();
+                bool signFlag = resHandler.IsTenpaySign();
+                if (signFlag && return_code.ToUpper() == "SUCCESS")
+                {
+                    string attach = resHandler.GetParameter("attach");
+                    string device_info = resHandler.GetParameter("device_info");
+                    string out_trade_no = resHandler.GetParameter("out_trade_no");
+                    string transaction_id = resHandler.GetParameter("transaction_id");
+                    string total_fee = resHandler.GetParameter("total_fee");
+
+                    // PayOrderNotifyParam param = new PayOrderNotifyParam()
+                    // {
+                    //     PayNo = out_trade_no,
+                    //     Attach = attach,
+                    //     PayPrice = decimal.Parse(total_fee) / 100,
+                    //     TransactionNo = transaction_id,
+                    // };
+
+                    //回调参数说明：https://pay.weixin.qq.com/wiki/doc/api/app/app.php?chapter=9_7&index=3
+                    //Logger.Info(string.Format("支付回调接收参数成功，notifyXml={0},param={1}", notifyXml, param.SerializeObject()));
+
+                    // result = Service.PayNotifyUrl(param);
+
+                    //正确的订单处理
+                    TodoApi.Program.money.付款后更新订单(total_fee, out_trade_no);
+                    TodoApi.Program.tool.输出log记录("TodoApi.Program.money.付款后更新订单 ok");
 
 
-                //正确的订单处理
-                TodoApi.Program.money.付款后更新订单(total_fee, out_trade_no);
-                TodoApi.Program.tool.输出log记录("TodoApi.Program.money.付款后更新订单 ok");
+
+                    string xml = string.Format(@"<xml>
+       <return_code><![CDATA[{0}]]></return_code>
+       <return_msg><![CDATA[{1}]]></return_msg>
+       </xml>", return_code, return_msg);
+
+                    return Content(xml, "text/xml");
+
+                }
+                else
+                {
+                    TodoApi.Program.tool.输出log记录("TodoApi.Program.money.付款后更新订单 验证失败");
+                    Console.Write(" 验证失败");
+                    //错误的订单处理
+                    //  Logger.Error(string.Format("支付回调接失败，signFlag={0},notifyXml={1}", signFlag, notifyXml));
+                }
+
+
             }
-            else
+            catch (Exception ex)
             {
-                res = "wrong";
-                TodoApi.Program.tool.输出log记录("TodoApi.Program.money.付款后更新订单 失败");
+                Console.Write(" 验证失败2="+ex);
+                //   Logger.Error(string.Format("支付回调异常：Message={0}，StackTrace={1}", ex.Message, ex.StackTrace));
+            }
 
-                //错误的订单处理
-            }
-
-            return res;
+            return Content("fail", "text/xml");
         }
+        //         [HttpPost]
+        //         public string WxNotify()
+        //         {
+        //              Console.WriteLine("HttpContext="+HttpContext.ToString());
+        //             ResponseHandler resHandler = new ResponseHandler(HttpContext);
+
+
+        //             string return_code = resHandler.GetParameter("return_code");
+
+        //             string return_msg = resHandler.GetParameter("return_msg");
+        //             string out_trade_no = resHandler.GetParameter("out_trade_no");//订单号
+        //             string total_fee = resHandler.GetParameter("total_fee");//订单金额，单位分
+        //             total_fee = (Convert.ToDecimal(total_fee) / 100).ToString("#0.00");//订单金额，单位元
+
+        //             string res = null;
+
+
+        //             resHandler.SetKey("z76h28p279z76h28p279kidjs7dh82k1");
+        //             //验证请求是否从微信发过来（安全）
+        //             if (resHandler.IsTenpaySign())
+        //             {
+        //                 res = "success";
+
+
+        //                 //正确的订单处理
+        //                 TodoApi.Program.money.付款后更新订单(total_fee, out_trade_no);
+        //                 TodoApi.Program.tool.输出log记录("TodoApi.Program.money.付款后更新订单 ok");
+        //             }
+        //             else
+        //             {
+        //                 res = "wrong";
+        //                 TodoApi.Program.tool.输出log记录("TodoApi.Program.money.付款后更新订单 失败");
+
+        //                 //错误的订单处理
+        //             }
+
+        //             return res;
+        //         }
     }
 }
